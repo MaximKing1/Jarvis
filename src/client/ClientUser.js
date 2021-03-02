5c90d2cae2fe63f6452ad42e5bdf49c5213001b6
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-03-02 06:59:44 +0000

    Update ClientUser.js

--- src/client/ClientUser.js
+++ src/client/ClientUser.js
@@ -1,1 +1,12 @@
-
+export class ClientUser {
+    constructor() {
+        this.username,      //string
+        this.discriminator, //string
+        this.verified,      //boolean
+        this.id,            //string
+        this.flags,         //number
+        this.email,         //string
+        this.bot,           //boolean
+        this.avatar         //string
+    }
+}
